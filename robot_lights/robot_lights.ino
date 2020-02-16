1cf6206361de958078d5675afa23e77add825c8c
AwesomeMiner291 <brennanpatrick20@gmail.com>
2020-02-15 21:13:39 -0500

    Finished ball code

--- robot_lights/robot_lights.ino
+++ robot_lights/robot_lights.ino
@@ -1,34 +1,46 @@
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
-#define NUM 144
-#define PIN 4
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM, PIN, NEO_GRB + NEO_KHZ800);
-#define NUM2 5
-#define PIN2 4
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(NUM2, PIN2, NEO_GRB + NEO_KHZ800);
+
+#define N 144
+#define P 4
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N, P, NEO_GRB + NEO_KHZ800);
+
+#define N2 60
+#define P2 5
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(N2, P2, NEO_GRB + NEO_KHZ800);
+
+/*#define PIN3 6
+Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(N, PIN3, NEO_GRB + NEO_KHZ800);*/
+
+/*#define N4 20
+#define PIN4 6
+Adafruit_NeoPixel strip4 = Adafruit_NeoPixel(N4, PIN4, NEO_GRB + NEO_KHZ800);*/
+
 int x=0;
 int y=1;
 int randompix;
 int randompix2;
 int randompix3;
-char input;
+int input;
+String binin;
 int RM;
 int GM;
 int BM;
 int RS;
 int GS;
 int BS;
+int balls = 0;
 
 
 void randomizer(){
   if(x==0){
-  randompix=random(0, NUM);
-  randompix2=random(0, NUM);
-  randompix3=random(0, NUM);
+  randompix=random(0, N);
+  randompix2=random(0, N);
+  randompix3=random(0, N);
   }
 }
-void fade(){
-  for(int i=0; i<NUM; i++){
+
+void fade(){  for(int i=0; i<N; i++){
     strip.setPixelColor(i,RM,GM,BM);
     strip.setBrightness(x);
   }
@@ -37,7 +49,7 @@ void fade(){
   strip.setPixelColor(randompix3,RS,GS,BS);
   if(y==1){
   x++;
-  if(x==255){
+  if(x==127){
     y=0;
   }
 }
@@ -48,49 +60,81 @@ else{
   }
 }
 }
-void defineinput(){
-  if(Wire.available() >0){
-    input=Wire.read();
+
+void readInput(int numBytes){
+  if( 0 < Wire.available()){
+    binin = Wire.read();
+    input = binin.toInt();
+    
   }
+  Serial.print(input);
+    
+  switch(input){
+    case 114:
+      RM=127;
+      GM=0;
+      BM=0;
+      RS=15;
+      GS=0;
+      BS=127;
+      break;
+    case 98:
+      RM=15;
+      GM=0;
+      BM=127;
+      RS=127;
+      GS=0;
+      BS=0;
+      break;
+    //case 108:
+      //lightsaber();
+      //break;
+    case 0:
+      balls = 0;
+      for(int i = 0; i < N2; i++){
+        strip2.setPixelColor(i, 0,0,0);
+      }
+      break;
+    case 01:
+      balls = 1;
+      ball();
+      break;
+    case 10:
+      balls = 2;
+      ball();
+      break;
+    case 11:
+      balls = 3;
+      ball();
+      break;
+    case 100:
+      balls = 4;
+      ball();
+      break;
+    case 101:
+      balls = 5;
+      ball();
+      break;
+  }
+  
 }
-void mainColor(){
-  if(input=="r"){
-    RM=127;
-    GM=0;
-    BM=0;
-    RS=15;
-    GS=0;
-    BS=127;
-  }
-  if(input=='b'){
-    RM=15;
-    GM=0;
-    BM=127;
-    RS=127;
-    GS=0;
-    BS=0;
-  }
-}
+
 void ball(){
-  for(int i=0; i<NUM2;i++){
-    strip.setPixelColor(i,127,0,0);
-  }
-  if(input=='1'){
-    for(int i=0; i<NUM2/5; i++){
-      strip.setPixelColor(i,0,0,0);
+    strip2.clear();
+   for(int i = 0; i < (N2*balls)/5; i++){
+     strip2.setPixelColor(i, 255,255,255);
+ //    strip3.setPixelColor(i,255,255,255);
+     Serial.print(i);
     }
-  }
-  if(input=='2'){
-    for(int i=0; i<(NUM2*2)/5;i++){
-      strip.setPixelColor(i,0,0,0);
-    }
-  }
-  if(input=='3'){
-    for(int i=0; i<(NUM2*3)/5;i++){
-      strip.setPixelColor
-    }
-  }
 }
+/*void lightsaber(){
+  for(int i=0; i<N4; i=i+2){
+    strip4.setPixelColor(i,RM,GM,BM);
+    strip4.show();
+    delay(2);
+  }
+}*/
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -99,25 +143,20 @@ strip.begin();
 strip.show();
 strip2.begin();
 strip2.show();
-strip. setBrightness(40);
+//strip3.begin();
+//strip3.show();
+//strip4.begin();
+//strip4.show();
 Wire.begin(4);                // join i2c bus with address #4
-//mainColor();
-RM=15;
-GM=0;
-BM=127;
-RS=127;
-GS=0;
-BS=0;
+Wire.onReceive(readInput);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-defineinput();
 randomizer();
 fade();
-ball();
 strip.show();
 strip2.show();
-
+//strip3.show();
 delay(1);
 }
